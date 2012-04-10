f4e00ea44ef6c1cf98d7c58506081e48b214c805
Radovan Dvorsky <rdvorsky6@gmail.com>
2012-04-10 23:06:18 +0200

    uprava testovacieho skriptu

--- time.sh
+++ time.sh
@@ -5,6 +5,8 @@ sucet(){ echo "$1+$2" | bc;}
 #Pocet kroku pro kazdy vstu[
 steps=$1
 time=0.0
+sum_nums=0
+sum_time=0
 
 #Odstrani obsah vystupni slozky
 rm -f ./times.out
@@ -19,19 +21,31 @@ for name in $names
 do
 	#Kopie do lattice
 	cp IN/$name lattice
+	echo "------------------------------------------"
 	echo $name
 	#Vystupni soubor
-	./test $steps >> ./times.out
+	./test $steps > ./times.out
+	nums=`cat ./times.out | grep '[^ ]' | wc -l`
+
+	while read line        
+	do        
+	    time=$(sucet $line $time);
+	done < ./times.out
+	sum_nums=$(sucet $sum_nums $nums)
+	sum_time=$(sucet $sum_time $time)
+	echo "CPUs cas: $time s"
+	echo "Pocet riadkov:" $nums
+	echo "Priemerny cas: `echo "$time/$nums" | bc -l` s"
 done
-while read line        
-do        
-    time=$(sucet $line $time);
-done < ./times.out
 
-nums=`cat ./times.out | grep '[^ ]' | wc -l`
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Celkovy CPUs cas: $sum_time s";
+echo "Celkovy pocet riadkov: $sum_nums";
+echo "Primerny cas: `echo " $sum_time/$sum_nums" | bc -l` s"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-echo "Celkovy cas: $time"
-echo "Pocet hodnot:" $nums
-echo "$time/$nums" | bc -l;
+
+
+
 
 
