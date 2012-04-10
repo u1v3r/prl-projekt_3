d06703aa2e46d01966b9c6067664a4d69425bd88
Radovan Dvorsky <rdvorsky6@gmail.com>
2012-04-10 17:55:49 +0200

    oprava chyby-hotovo

--- ca.c
+++ ca.c
@@ -10,7 +10,7 @@
 /* vypocita nasledujucu interaciu */
 void play(int *line,int *up_line,int *down_line,int cols,int rows,int myid,int *result_line);
 /* vypise riadok na vystup */
-void print_row(int *line,int length,int myid);
+void print_row(int *line,int length);
 inline int calculate_cell(int *line,int *up_line,int *down_line,int cols,int i);
 inline void send_value(int value);
 void swap_pointers(int **a,int **b);
@@ -108,24 +108,22 @@ int main(int argc, char *argv[]){
 
     fclose(f);
 
-
-    /* vypocet interacii hry */
+    /* vypocet iteracii hry */
     for(i = 0; i < reapeat; i++){
         for(j = 0; j < cols; j++){
             result_line[j] = calculate_cell(line,up_line,down_line,cols,j);
 
-                MPI_Request req;
+            MPI_Request req;
 
-                /* ak je posledny tak dalej neposielaj */
-                if(myid != (lines_count - 1)){
-                    MPI_Isend(&result_line[j], 1, MPI_INT, (myid + 1), TAG, MPI_COMM_WORLD,&req);
-                }
-                /* ak je prvy tak neposiela na prechadzajuci */
-                if(myid > 0){
-                    MPI_Isend(&result_line[j], 1, MPI_INT, (myid - 1), TAG, MPI_COMM_WORLD,&req);
-                }
+            /* ak je posledny tak dalej neposielaj */
+            if(myid != (lines_count - 1)){
+                MPI_Isend(&result_line[j], 1, MPI_INT, (myid + 1), TAG, MPI_COMM_WORLD,&req);
+            }
 
-
+            /* ak je prvy tak neposiela na prechadzajuci */
+            if(myid > 0){
+                MPI_Isend(&result_line[j], 1, MPI_INT, (myid - 1), TAG, MPI_COMM_WORLD,&req);
+            }
 
             //send_value(result_line[j]);
             /* prvy neprijima od predchadzajuceho */
@@ -139,9 +137,9 @@ int main(int argc, char *argv[]){
             }
 
             /* obidve hodnoty su odoslane */
-                MPI_Wait(&req,&stat);
+            MPI_Wait(&req,&stat);
         }
-        /*play(line,up_line,down_line,cols,lines_count,myid,result_line);*/
+
         //MPI_Barrier(MPI_COMM_WORLD);/* treba pockat na dopocitanie hodnot */
         /* v dalsom kole bude pocitat s vysledkom */
         swap_pointers(&result_line,&line);
@@ -150,7 +148,7 @@ int main(int argc, char *argv[]){
         swap_pointers(&down_line,&down_line_tmp);
     }
 
-    print_row(line,cols,myid);
+    print_row(line,cols);
 
     MPI_Finalize();
     return 0;
@@ -181,7 +179,7 @@ inline void send_value(int value){
 
 inline int calculate_cell(int *line,int *up_line,int *down_line,int cols,int i){
 
-        int value;
+        int value = line[i];
         int counter = 0;
 
         /* okrajove hodnoty matice */
@@ -243,7 +241,7 @@ inline int calculate_cell(int *line,int *up_line,int *down_line,int cols,int i){
             /* posledny riadok prvy stlpec */
             if(myid == (lines_count - 1) && i == 0){
                 if(up_line[i]) counter++;/*hodnota hore*/
-                if(down_line[i+1]) counter++;/*vpravo dole */
+                if(up_line[i+1]) counter++;/*vpravo dole */
                 if(line[i+1]) counter++;/* vpravo */
             }
 
@@ -274,11 +272,11 @@ inline int calculate_cell(int *line,int *up_line,int *down_line,int cols,int i){
             if(counter < 2) value = 0;/* Každá živá buňka s méně než dvěma živými sousedy umírá. */
             else if(counter == 2 || counter == 3) value = 1;/* Každá živá buňka se dvěma nebo třemi živými sousedy zůstává žít. */
             else if(counter > 3) value = 0;/* Každá živá buňka s více než třemi živými sousedy umírá. */
-            else value = line[i];
+            //else value = line[i];
 
         }else{/*mrtva*/
-            if(counter == 3) value=1;/*Každá mrtvá buňka s právě třemi živými sousedy ožívá.*/
-            else value = 0;
+            if(counter == 3) value = 1;/*Každá mrtvá buňka s právě třemi živými sousedy ožívá.*/
+            //else value = 0;
         }
 
         //printf("row:%d cell:%d value:%d counter:%d\n",myid,i,value,counter);
@@ -286,124 +284,7 @@ inline int calculate_cell(int *line,int *up_line,int *down_line,int cols,int i){
         return value;
 }
 
-
-void play(int *line,int *up_line,int *down_line,int cols,int rows, int myid,int *result_line){
-
-    int i;
-    int counter = 0;/*pocet zivich buniek*/
-
-    /* prechadzaj cely riadok a nastav hodnotu (0|1)*/
-    for(i = 0; i < cols; i++){
-
-        /* okrajove hodnoty matice */
-        if( myid == 0 || i == 0 || myid == (rows - 1) || i == (cols - 1)) {
-
-            /* je to prvy riadok alebo posledny riadok */
-            if(myid == 0 || myid == (rows - 1)){
-                /* a nie je to prvy stlpec a posledny stlpec */
-                if(i != 0 && i != (cols - 1)){
-
-                    if(myid == 0){/* prvy riadok ma len dolne */
-                        if(down_line[i]) counter++;/*hodnota dole*/
-                        if(down_line[i+1]) counter++;/*vpravo dole */
-                        if(down_line[i-1]) counter++;/*vlavo dole */
-                    }else{/* posledny len horne */
-                        if(up_line[i]) counter++;/*hodnota hore*/
-                        if(up_line[i+1]) counter++;/*vpravo hore */
-                        if(up_line[i-1]) counter++;/*vlavo hore */
-                    }
-
-                    if(line[i+1]) counter++;/* vpravo */
-                    if(line[i-1]) counter++;/* vlavo */
-                }
-            }
-
-            if(i == 0 || i == (cols - 1)){/* je to prvy alebo posledny stlpec */
-
-                /* a nie je to prvy riadok alebo posledny riadok */
-                if(myid != 0 && myid != (rows - 1)){
-                    /* prvy stlpec */
-                    if(i == 0){
-                        if(up_line[i+1]) counter++;/*vpravo hore */
-                        if(down_line[i+1]) counter++;/*vpravo dole */
-                        if(line[i+1]) counter++;/* vpravo */
-                    }else{/* posledny stlpec */
-                        if(down_line[i-1]) counter++;/*vlavo dole */
-                        if(up_line[i-1]) counter++;/*vlavo hore */
-                        if(line[i-1]) counter++;/* vlavo */
-                    }
-
-                    if(up_line[i]) counter++;/*hodnota hore*/
-                    if(down_line[i]) counter++;/*hodnota dole*/
-                }
-            }
-
-            /* prvy riadok prvy stlpec */
-            if(myid == 0 && i == 0){
-                if(down_line[i]) counter++;/*hodnota dole*/
-                if(down_line[i+1]) counter++;/*vpravo dole */
-                if(line[i+1]) counter++;/* vpravo */
-            }
-
-            /* prvy riadok posledny stlpec*/
-            if(myid == 0 && i == (cols - 1)){
-                if(line[i-1]) counter++;/* vlavo */
-                if(down_line[i-1]) counter++;/*vlavo dole */
-                if(down_line[i]) counter++;/*hodnota dole*/
-            }
-
-            /* posledny riadok prvy stlpec */
-            if(myid == (rows - 1) && i == 0){
-                if(up_line[i]) counter++;/*hodnota hore*/
-                if(down_line[i+1]) counter++;/*vpravo dole */
-                if(line[i+1]) counter++;/* vpravo */
-            }
-
-            /* posledny riadok posledny stlpec */
-            if(myid == (rows - 1) && i == (cols - 1)){
-                if(line[i-1]) counter++;/* vlavo */
-                if(up_line[i-1]) counter++;/*vlavo hore */
-                if(up_line[i]) counter++;/*hodnota hore*/
-            }
-
-        }else {/* stredne hodnoty matice bez okrajovych */
-            if(down_line[i]) counter++;/*hodnota dole*/
-            if(down_line[i+1]) counter++;/*vpravo dole */
-            if(down_line[i-1]) counter++;/*vlavo dole */
-
-            if(up_line[i]) counter++;/*hodnota hore*/
-            if(up_line[i+1]) counter++;/*vpravo hore */
-            if(up_line[i-1]) counter++;/*vlavo hore */
-
-            if(line[i+1]) counter++;/* vpravo */
-            if(line[i-1]) counter++;/* vlavo */
-
-        }
-
-
-
-        /* je ziva */
-        if(line[i]){
-            if(counter < 2)result_line[i] = 0;/* Každá živá buňka s méně než dvěma živými sousedy umírá. */
-            else if(counter == 2 || counter == 3)result_line[i]=1;/* Každá živá buňka se dvěma nebo třemi živými sousedy zůstává žít. */
-            else if(counter > 3) result_line[i]=0;/* Každá živá buňka s více než třemi živými sousedy umírá. */
-            else result_line[i]=line[i];
-
-        }else{/*mrtva*/
-            if(counter == 3) result_line[i]=1;/*Každá mrtvá buňka s právě třemi živými sousedy ožívá.*/
-            else result_line[i]=line[i];
-        }
-
-
-        counter = 0;
-    }
-
-
-
-    return;
-}
-
-void print_row(int *line,int length,int myid){
+void print_row(int *line,int length){
 
     int i;
 
